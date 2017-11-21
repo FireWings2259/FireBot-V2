4fbf30bbee72a27343e5327bcb7c2f5547303a5c
FireWing <FireWings2259@users.noreply.github.com>
2017-11-21 08:50:57 +0000

    Push new files

---
+++ bot.js
@@ -0,0 +1,18 @@
+//FireBot v2
+//By FireWings
+
+const authSettings = require("./config.json");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login(authSettings.bot.token);
